8517907c2b0454ec48e7237fcfa296dd29f57a1e
a <alexeybulmeni@gmail.com>
2020-09-04 18:18:20 -0700

    Friendlier mobile

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Smoothly transitions the title colors
+
 const randomColor = () => '#' + Math.floor(Math.random()*16777215).toString(16)
 const changeColor = (id_type) => document.getElementById(id_type).style.color = randomColor()
 
@@ -13,3 +15,20 @@ setInterval(() => {
 // start color animation as soon as document is ready
 setTimeout(changeColor, 10000)
 
+// Draws the grid above/below instead of left/right when the site is thin enough
+
+
+function resize_grid() {
+  var ratio = window.innerWidth / window.innerHeight
+  var grid = document.getElementsByClassName("grid-container")
+
+  if (ratio < 1) {
+    grid[0].style.display = "block"
+  } else {
+    grid[0].style.display = "grid"
+  }
+}
+
+
+resize_grid()
+window.onresize = (resize_grid)

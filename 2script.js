4c581ac5aff29d3148aa0c26ffe5b72722885cca
a <alexeybulmeni@gmail.com>
2020-09-09 17:23:22 -0700

    Bugfixes for functions

--- 2script.js
+++ 2script.js
@@ -20,9 +20,8 @@ input.addEventListener("keyup", function(event) {
 
 // Post message (no >>>)
 function Post(message, wait=0) {
-  var terminal_text = document.getElementById("terminal_alltext").innerHTML 
-
   function dostuff() {
+    var terminal_text = document.getElementById("terminal_alltext").innerHTML 
     document.getElementById("terminal_alltext").innerHTML = terminal_text +  '<br>' + "<span class='bot_text'>" + message + '</span>'
   }
   setTimeout(dostuff, wait);
@@ -33,24 +32,19 @@ function Post(message, wait=0) {
 
 
 function Edit(to_edit, wait=0) {
-  // array with each element was split by <br>
-  var text = document.getElementById("terminal_alltext").innerHTML 
-  var text_array = text.split('<br>')
-  before_edit = text_array.pop()                              // deletes last message
-  to_edit = "<span class='bot_text'>" + to_edit + '</span>'   // formatting
-  text_array.push(to_edit)                                    // Adds edit
-  var edited_text = text_array.join("<br>")                   // Joins array into string
-
-
-  // this thing maks a builtin wait, so it waits x millisecs until editing
-  // I have no idea how there is no problems with global/local variables, but dont question it
 
   function dostuff() {
+    var text = document.getElementById("terminal_alltext").innerHTML 
+    var text_array = text.split('<br>')                                 // list with each line seperated by <br>
+    text_array.pop()                                                    // deletes last message
+    to_edit = "<span class='bot_text'>" + to_edit + '</span>'           // formatting
+    text_array.push(to_edit)                                            // Adds edit
+    var edited_text = text_array.join("<br>")                           // Joins array into string  
     document.getElementById("terminal_alltext").innerHTML = edited_text // Modifies the html
   }
   setTimeout(dostuff, wait);
 
-  var return_message = 'edit(' + before_edit + ' --> ' + to_edit + ')'
+  var return_message = 'message edited'
   return return_message
 }
 
@@ -58,24 +52,23 @@ function Edit(to_edit, wait=0) {
 
 // Deletes X message (default is 1)
 function Delete(how_many=1, wait=0) {
-  // array with each element was split by <br>
-  var text = document.getElementById("terminal_alltext").innerHTML 
-  var text_array = text.split('<br>')
-
-  if (Number.isInteger(how_many)) { // if inputted an integer, delete that many message
-  var count = 0
-  while (count < how_many) {
-    text_array.pop()
-    count++
-  }
-  } else if (how_many=="max") { // all messages
-    text_array = ['']
-  } else if (how_many=='first') { //first message
-    text_array[0] = ''
-  }
-  var edited_text = text_array.join("<br>")
-
+  
   function dostuff() {
+    var text = document.getElementById("terminal_alltext").innerHTML 
+    var text_array = text.split('<br>')
+  
+    if (Number.isInteger(how_many)) { // if inputted an integer, delete that many message
+    var count = 0
+    while (count < how_many) {
+      text_array.pop()
+      count++
+    }
+    } else if (how_many=="max") { // all messages
+      text_array = ['']
+    } else if (how_many=='first') { //first message
+      text_array[0] = ''
+    }
+    var edited_text = text_array.join("<br>")
     document.getElementById("terminal_alltext").innerHTML = edited_text
   }
   setTimeout(dostuff, wait);
@@ -85,33 +78,33 @@ function Delete(how_many=1, wait=0) {
 }
 
 function terminal_init() {
-  setTimeout(function(){{Post("Installing HACKORZ.exe...")}; }, 50);
-  setTimeout(function(){{Post("Installing Encode.exe")}; }, 250);
-  setTimeout(function(){{Edit("Installing Encode.exe.")}; }, 500);
-  setTimeout(function(){{Edit("Installing Encode.exe..")}; }, 666);
-  setTimeout(function(){{Edit("Installing Encode.exe...")}; }, 833);
+  Post("Installing HACKORZ.exe...", 50);
+  Post("Installing Encode.exe", 250)
+  Edit("Installing Encode.exe.", 500)
+  Edit("Installing Encode.exe..", 666)
+  Edit("Installing Encode.exe...", 833)
 
-  setTimeout(function(){{Edit("Installing Decode.exe")}; }, 1000);
-  setTimeout(function(){{Edit("Installing Decode.exe.")}; }, 1250);
-  setTimeout(function(){{Edit("Installing Decode.exe..")}; }, 1500);
-  setTimeout(function(){{Edit("Installing Decode.exe...")}; }, 1750);
+  Edit("Installing Decode.exe", 1000)
+  Edit("Installing Decode.exe.", 1250)
+  Edit("Installing Decode.exe..", 1500)
+  Edit("Installing Decode.exe...", 1750)
 
   var session_id = [...Array(30)].map(() => Math.random().toString(36)[2]).join('')
-  setTimeout(function(){{Post("Connecting to valid session...")}; }, 2500);
-  setTimeout(function(){{Edit("Session found!")}; }, 3000);
-  setTimeout(function(){{Post(session_id)}; }, 3001);
+  Post("Connecting to valid session...", 2500)
+  Edit("Session found!", 3000)
+  Post(session_id, 3001)
 
-  setTimeout(function(){{Post('Finding token...')}; }, 3200);
-  setTimeout(function(){{Edit("Token found!")}; }, 3800);
+  Post('Finding token...', 3200)
+  Edit("Token found!", 3800)
 
-  setTimeout(function(){{Post([...Array(60)].map(() => Math.random().toString(36)[2]).join(''))}; }, 3900);
-  setTimeout(function(){{Post("Sending encrypted data key for HACKORZ.exe")}; }, 4000);
-  setTimeout(function(){{Post("⌟ᶆᶲᶱᶪᶵᶤᶷᶶᵤᵣᶜᶲᶸᵣᶩᶲᶸᶱᶧᵣᶤᶱᵣᶨᶤᶶᶷᶨᶵᵣᶨᶪᶪᵤᵣᵫᵴᵲᵴᵳᵬ")}; }, 4500);
-  setTimeout(function(){{Post("Done! You will be able to write shortly!")}; }, 5000);
-  setTimeout(function(){{Delete("max")}; }, 7000);
+  Post([...Array(60)].map(() => Math.random().toString(36)[2]).join(''), 3900)
+  Post("Sending encrypted data key for HACKORZ.exe", 4000)
+  Post("⌟ᶆᶲᶱᶪᶵᶤᶷᶶᵤᵣᶜᶲᶸᵣᶩᶲᶸᶱᶧᵣᶤᶱᵣᶨᶤᶶᶷᶨᶵᵣᶨᶪᶪᵤᵣᵫᵴᵲᵴᵳᵬ", 4500)
+  Post("Done! You will be able to write shortly!", 5000)
+  Delete("max", 7000)
   
   document.getElementById("command").readOnly = false;
-  document.getElementById(terminal_alltext).innerHTML = "Welcome to Encrypt and Decrypt machine, or know more simply as DAE!"
+  document.getElementById('terminal_alltext').innerHTML = "Welcome to Encrypt and Decrypt machine, or know more simply as DAE!"
 }
 
 

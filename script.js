ab96d200a5e6d251fe235729866add00df8257d2
a <alexeybulmeni@gmail.com>
2020-09-27 15:47:23 -0700

    Some new stuff ;)

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Smoothly transitions the title colors
 
-const randomColor = () => '#' + Math.floor(Math.random()*16777215).toString(16)
-const changeColor = (id_type) => document.getElementById(id_type).style.color = randomColor()
+const randomColor = () => '#' + Math.floor(Math.random()*16777215).toString(16);
+const changeColor = (id_type) => document.getElementById(id_type).style.color = randomColor();
 
 
 setInterval(() => {
-  changeColor("t1")
-  changeColor("t2")
-  changeColor("t3")
-  changeColor("t4")
+  changeColor("t1");
+  changeColor("t2");
+  changeColor("t3");
+  changeColor("t4");
 }, 5000)
 
 
@@ -19,15 +19,20 @@ setTimeout(changeColor, 10000)
 
 
 function resize_grid() {
-  var grid = document.getElementsByClassName("grid-container")
+  var grid = document.getElementsByClassName("grid-container");
 
   if (window.innerWidth <  window.innerHeight) {
-    grid[0].style.display = "block"
+    grid[0].style.display = "block";
   } else {
-    grid[0].style.display = "grid"
+    grid[0].style.display = "grid";
   }
 }
 
+var secret_button = document.getElementById("t3");
+var secret_text = document.getElementById('title_info')
+secret_button.onclick = function() {
+  secret_text.innerHTML = 'Wow! Why did you even click on this? id: thats_a_button'
+}
 
-resize_grid()
-window.onresize = (resize_grid)
+resize_grid();
+window.onresize = (resize_grid);

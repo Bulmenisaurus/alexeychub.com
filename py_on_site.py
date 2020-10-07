afc67e73ea0d2c71b715adad095bbc143183e173
a <alexeybulmeni@gmail.com>
2020-10-06 22:18:19 -0700

    lang=en and viewport

--- py_on_site.py
+++ py_on_site.py
@@ -1,7 +1,3 @@
-from pygments import highlight
-from pygments.lexers import PythonLexer
-from pygments.formatters import HtmlFormatter
-
-code = 'from lmao import lm\ner = \'0xe12e212\'\ndef func()\n\tprint(\'lol\')'
-print(highlight(code, PythonLexer(), HtmlFormatter()))
-#print(HtmlFormatter().get_style_defs('.highlight'))
+print(f"{''}")
+if 3 == 3:
+    print('lol')

dbf582ea2f119b27f2b340fd3ac3caeb1188866b
a <alexeybulmeni@gmail.com>
2020-12-21 09:10:43 -0800

    Continuing to work on secrets ;)

--- JS_files/secrets.js
+++ JS_files/secrets.js
@@ -1,16 +1,26 @@
-var clicks = 0
-
-
 // If you have not found at least 5 easter eggs, please do not continue.
 
+
+var clicks = 0;
+var showBorder = true;
+var canCreate = true; // can another easter egg be created?
+
 function mainClick() {
-    clicks ++
-    console.log(clicks)
+    clicks++;
+    canCreate = false
 }
 
-setTimeout(delay_egg, 10000)
+setTimeout(function(){canCreate && (document.querySelector("#item-4").innerHTML="<p>Wow, you're patient! id: kinda_lazy</p>");},(120*1000));
 
-function delay_egg() {
-    document.querySelector("#results").innerHTML = "Wow, you're patient!";
-    console.log("done!");
-}
+document.addEventListener('keyup', function (event) {
+    // event.key is key pressed
+    if (['`', '~'].includes(event.key)) {
+        showBorder ^= true;
+        elements = document.getElementsByClassName('grid-item');
+        border_style = "1px solid " + (showBorder ? "white" : "black");
+
+        for (x = 0; x < elements.length; x++){
+            elements[x].style.border = border_style;
+        };
+    };
+});

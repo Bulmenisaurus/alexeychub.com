3ce803589f5de7c8e8585bc2b5fdbbde45985705
a <alexeybulmeni@gmail.com>
2020-12-25 19:04:44 -0800

    Majorly reworking one line wonders

--- tools/py_on_site.py
+++ tools/py_on_site.py
@@ -5,5 +5,5 @@ from pygments.formatters import HtmlFormatter
 print('\033[0m')
 #print(f"\033[35m{'happy b-day {your name here}'.capitalize()}\033[90m\n{chr(60)+(l_1 := '------~~~~~~**') + l_1[::-1]+chr(62)}\033[0m\n\n{'Your message here'}")
 #print(f"\n\n{(message := ' '+f'Happy Valetine{chr(39)}s day, Hobbert!')}\n{(line := (hrt := '💕')+'—'*len(message)+hrt)}\nLorizzle ipsizzle you son of a bizzle shizzlin dizzle gangster,\nthe bizzle adipiscing elit. Nullam stuff its fo rizzle, fo\nshizzle volutpizzle,suscipit ghetto, gravida crunk, crazy.\n\n{line}\n— \033[1mEnt0_3301\033[0m, with love")
-code = "'Happy B-day {your name here}!'.capitalize()"
+code = "print(\"abc\")"
 print(highlight(code, PythonLexer(), HtmlFormatter()))

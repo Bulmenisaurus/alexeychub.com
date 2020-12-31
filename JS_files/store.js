c4cfe00ea936a2f94107f835779384bdadc6c8a7
a <alexeybulmeni@gmail.com>
2020-12-30 17:58:10 -0800

    new formDataToQuery() function

--- JS_files/store.js
+++ JS_files/store.js
@@ -1,5 +1,14 @@
+const formDataToQuery = (formData) => {
+    [...formData.entries()]
+        .map(x => `${encodeURIComponent(x[0])}=${encodeURIComponent(x[1])}`)
+        .join('&');
+}
+
+
 function generateFormLink() {
     const form = document.getElementById('card-form');
-    let formData = new FormData(form)
-    console.log(...formData.entries())
+    let formData = new FormData(form);
+    formData = formDataToQuery(formData);
+
+    console.log(formData);
 }

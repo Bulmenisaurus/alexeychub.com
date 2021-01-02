bff7f12845fc4187abe25dfb73081c8afc5ffced
a <alexeybulmeni@gmail.com>
2021-01-01 17:21:05 -0800

    Prototype of result

--- JS_files/storeresult.js
+++ JS_files/storeresult.js
@@ -6,9 +6,10 @@ const text = urlParams.get('t') || '';
 
 const imageUrl = `https://alexeychub.com/images/store_${hearts}.jpeg`;
 
-let image = document.getElementById('image').appendChild(document.createElement('img')) // = `<img src="${imageUrl}" alt="image_${hearts}">`;
-image.src = 'images/kitty.jpeg' // imageUrl
-image.alt = 'A fox and a bear sitting together.'
+let image = document.getElementById('image').appendChild(document.createElement('img')); // = `<img src="${imageUrl}" alt="image_${hearts}">`;
+image.src = 'images/kitty.jpeg'; // imageUrl
+image.alt = 'A fox and a bear sitting together.';
+image.height = 185 * 2; image.width = 300 * 2;
 
 document.getElementById('text').innerHTML = text;
 

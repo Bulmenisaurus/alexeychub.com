08912d9fe86d7d5e509e4afe33cbaa7bfac22748
a <alexeybulmeni@gmail.com>
2021-01-02 13:38:24 -0800

    Using real/test images

--- JS_files/storeresult.js
+++ JS_files/storeresult.js
@@ -7,7 +7,7 @@ const text = urlParams.get('t') || '';
 const imageUrl = `https://alexeychub.com/images/store_${hearts}.jpeg`;
 
 let image = document.getElementById('image').appendChild(document.createElement('img')); // = `<img src="${imageUrl}" alt="image_${hearts}">`;
-image.src = 'images/placeholder_kitty_300:185.jpeg'; // imageUrl
+image.src = imageUrl; // imageUrl
 image.alt = 'A fox and a bear sitting together.';
 image.height = 185 * 2; image.width = 300 * 2;
 

8729715398e97d0b0942560af4fd4d37c891b77e
a <alexeybulmeni@gmail.com>
2021-01-02 17:00:30 -0800

    ajldalsk

--- JS_files/storeresult.js
+++ JS_files/storeresult.js
@@ -1,15 +1,20 @@
 // &h=3&t=hallo
 
 const urlParams = new URLSearchParams(window.location.search);
-const hearts = urlParams.get('h') || '0';
+const imageId = urlParams.get('h') || '0';
 const text = decodeURIComponent(urlParams.get('t') || '');
 
-const imageUrl = `https://alexeychub.com/images/store_${hearts}.jpg`;
+const imageUrl = `https://alexeychub.com/images/store_${imageId}.jpg`;
 
 let image = document.getElementById('image').appendChild(document.createElement('img')); // = `<img src="${imageUrl}" alt="image_${hearts}">`;
 image.src = imageUrl; // imageUrl
 //image.alt = 'A fox and a bear sitting together.';
 image.height = 185 * 2; image.width = 300 * 2;
+
+gtag('event', 'view-card', {
+    'event_label': imageId,
+});
+
 
 document.getElementById('text').innerHTML = text.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/\n/g, '<br>');
 

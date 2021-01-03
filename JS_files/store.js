25757c2a43cd1932eba8599529a4ffeaf4c04f44
a <alexeybulmeni@gmail.com>
2021-01-02 20:11:56 -0800

    More changes

--- JS_files/store.js
+++ JS_files/store.js
@@ -15,14 +15,14 @@ function generateFormLink() {
 };
 
 function updateLink() {
-    url = generateFormLink()
+    let url = generateFormLink()
     if (document.getElementById('modal-preview').style.display != 'block') document.getElementById('preview-iframe').src = url;
     document.getElementById('link-preview').innerText = url;
     document.querySelector("#copy-link-input").value = url;
 };
 
 function copyLink() {
-    copyInput = document.querySelector("#copy-link-input")
+    let copyInput = document.querySelector("#copy-link-input")
 
     /* Select the text field */
     copyInput.select();
@@ -31,10 +31,22 @@ function copyLink() {
     document.execCommand('copy');
 };
 
-const modal = document.querySelector('#modal-preview')
-const modalClose = document.querySelector('#close-preview')
-const modalTrigger = document.querySelector('#open-preview')
+/* Modal section */
+const modal = document.querySelector('#modal-preview');
+const modalClose = document.querySelector('#close-preview');
+const modalTrigger = document.querySelector('#open-preview');
 
-modalTrigger.onclick = function () { modal.style.display = 'block' }
-modalClose.onclick = function () { modal.style.display = 'none'; console.log('click clock the modal\'s done') }
-document.onclick = function (e) { if (e.target == modal) { modal.style.display = 'none' } }
+modalTrigger.onclick = function () { modal.style.display = 'block' };
+modalClose.onclick = function () { modal.style.display = 'none'; console.log('click clock the modal\'s done') };
+document.onclick = function (e) { if (e.target == modal) { modal.style.display = 'none' } };
+/* end Modal */
+
+const updateLen = function () {
+    let length = document.querySelector("#text").value.length;
+    document.querySelector('#chars-left').innerHTML = `(${length}/150)`;
+};
+
+updateLen();
+
+document.querySelector("#text").addEventListener('keyup', updateLen);
+document.querySelector("#text").addEventListener('keydown', updateLen);

e55c8878cef17416a4268b9838764d2a5aebd1c4
a <alexeybulmeni@gmail.com>
2021-01-05 21:50:31 -0800

    Cnage to eslint brace style and reduced severity to a warn

--- JS_files/script.js
+++ JS_files/script.js
@@ -5,39 +5,40 @@ const randomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16
 const changeColor = (id_type) => document.getElementById(id_type).style.color = randomColor();
 
 setInterval(() => {
-  changeColor("t1");
-  changeColor("t2");
-  changeColor("t3");
-  changeColor("t4");
-}, 5000)
+    changeColor('t1');
+    changeColor('t2');
+    changeColor('t3');
+    changeColor('t4');
+}, 5000);
 
 
 // Draws the grid above/below instead of left/right when the site is thin enough
 
 
 function resize_grid() {
-  var grid = document.getElementsByClassName("grid-container");
+    const grid = document.getElementsByClassName('grid-container')[0];
 
-  if (window.innerWidth < window.innerHeight) {
-    grid[0].style.display = "block";
-  } else if (grid[0].style.display) {
-    grid[0].style.display = "grid";
-  }
+    if (window.innerWidth < window.innerHeight) {
+        grid[0].style.display = 'block';
+
+    } else if (grid[0].style.display) {
+        grid[0].style.display = 'grid';
+    }
 }
 
-var secret_button = document.getElementById("t3");
-var secret_text = document.getElementById('title_info')
-click_amt = -10
-has_consoled = false
+var secret_button = document.getElementById('t3');
+var secret_text = document.getElementById('title_info');
+click_amt = -10;
+has_consoled = false;
 
 secret_button.onclick = function() {
-  secret_text.innerHTML = (click_amt >= 0) ? 'Why did you click on this even more? id: thats_definetely_a_button' : 'Wow! Why did you even click on this? id: thats_a_button'
-  click_amt++
-  if (secret_text.innerHTML == 'Why did you click on this even more? id: thats_definetely_a_button' && !has_consoled) {
-    console.log("Yup, the button does multiple things. Who knew? id: side_effects")
-    has_consoled = true
-  }
-}
+    secret_text.innerHTML = (click_amt >= 0) ? 'Why did you click on this even more? id: thats_definetely_a_button' : 'Wow! Why did you even click on this? id: thats_a_button';
+    click_amt++;
+    if (secret_text.innerHTML == 'Why did you click on this even more? id: thats_definetely_a_button' && !has_consoled) {
+        console.log('Yup, the button does multiple things. Who knew? id: side_effects');
+        has_consoled = true;
+    }
+};
 
 resize_grid();
 window.onresize = (resize_grid);

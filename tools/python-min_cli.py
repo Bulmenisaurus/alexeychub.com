e3d89621588361619fb82cb042ede33c28638fb6
a <alexeybulmeni@gmail.com>
2021-01-13 21:24:32 -0800

    Use minified css

--- tools/python-min_cli.py
+++ tools/python-min_cli.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 # from https://cssminifier.com/python
 # quick run:
-# for file in CSS_files/*; do python tools/python-min_cli.py "$file"; done 
+# for file in CSS_files/*; do python tools/python-min_cli.py "$file"; done
 
 import sys
 import requests
+
+if '.min.' in sys.argv[1]:
+    sys.exit(f'{sys.argv[1]} already minified')
 
 try:
     css_file = sys.argv[1]
@@ -16,14 +19,14 @@ except:
 with open(css_file, 'r') as c:
     css = c.read()
 
-# Pack it, ship it    
+# Pack it, ship it
 payload = {'input': css}
 url = 'https://cssminifier.com/raw'
 print("Requesting mini-me of {}. . .".format(c.name))
 r = requests.post(url, payload)
 
 # Write out minified version
-minified = (css_file.rstrip('.css')+'.min.css').replace("CSS_files/", "MIN_CSS_files/")
+minified = (css_file.removesuffix('.css')+'.min.css')
 with open(minified, 'w') as m:
     m.write(r.text)
 

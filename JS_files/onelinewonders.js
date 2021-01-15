ba1231482bef24e9af0934763c99881b8cd3af9f
a <alexeybulmeni@gmail.com>
2021-01-15 11:41:27 -0800

    Changed to js, fixed markup, and fixed code!

--- JS_files/onelinewonders.js
+++ JS_files/onelinewonders.js
@@ -1,33 +1,14 @@
-const [bName, bMessage, bPrev, vName, vMessage, vFrom] = ['bday-recipient', 'bday-message', 'bday-preview', 'valentine-name', 'valentine-message', 'valentine-from'].map((e) => document.getElementById(e));
-/*
-bName = document.getElementById('bday-recipient')
-bMessage = document.getElementById('bday-message')
-vName = document.getElementById('valentine-name')
-vMessage = document.getElementById('valentine-message')
-vFrom = document.getElementById('valentine-from')
-*/
+/* global hljs */
+const [bName, bPrev, vName, vMessage, vFrom] = ['bday-recipient', 'bday-preview', 'valentine-name', 'valentine-message', 'valentine-from'].map((e) => document.getElementById(e));
 
-function calcHtml(name, message) {
-    const n = name.replace(/\\n/g, 'chr(10)');
-    const m = message.replace(/\\n/g, 'chr(10)');
-    return {
-        'b': `<span></span><span class="nb">print</span><span class="p">(</span><span class="s2">f&quot;{'Happy B-day ${n}!'.capitalize()}\\n{chr(60)+(l_1 := '------~~~~~~**') + l_1[::-1]+chr(62)}\\n\\n{'${m}'}\\n\\n{chr(60)+l_1+l_1[::-1]+chr(62)}&quot;</span><span class="p">)</span>`,
-        'bp': `Happy b-day ${n}!<br>&lt;------~~~~~~****~~~~~~------&gt;<br><br>${m}<br><br>&lt;------~~~~~~****~~~~~~------&gt;`,
-    };
+function sanitize(string) {
+    const div = document.createElement('div');
+    div.innerText = string;
+    return div.innerHTML;
 }
 
-
-bName.onchange = function() {
-    const c = calcHtml(this.value, bMessage.value);
-    document.getElementById('bday-result').innerHTML = c.b;
-    bPrev.innerHTML = c.bp;
+bName.onkeyup = function() {
+    const name = sanitize(this.value);
+    document.getElementById('b-day').innerHTML = `Array(4).fill(["Happy birthday ","",""]).map((a,p)=>a.map((a,e)=>1==e&&2==p?"dear ${name}":1==e&&2!=p?"to you":2==e?",,!."[p]:a)).map(a=>a.join("")).concat("\\n"+"\\uD83C\\uDF81\\uD83C\\uDF89".repeat(5)).join("\\n");`;
+    hljs.highlightBlock(document.getElementById('b-day'));
 };
-bMessage.onchange = function() {
-    const c = calcHtml(bName.value, this.value);
-    document.getElementById('bday-result').innerHTML = c.b;
-    bPrev.innerHTML = c.bp;
-};
-
-vName.onchange = function() { document.getElementById('valentine_name').innerHTML = vName.value ? vName.value.replace(/\\n/g, 'chr(10)') : '{your name here}'; };
-vMessage.onchange = function() { document.getElementById('valentine_message').innerHTML = vMessage.value ? vMessage.value.replace(/\\n/g, 'chr(10)') : '{your message here}'; };
-vFrom.onchange = function() { document.getElementById('valentine_from').innerHTML = vFrom.value ? vFrom.value.replace(/\\n/g, 'chr(10)') : '{from who is it?}'; };

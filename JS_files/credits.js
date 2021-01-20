d736564183ce4aa58c2775874c20bd3300d16ab8
a <alexeybulmeni@gmail.com>
2021-01-20 13:28:05 -0800

    Added a recourse Im using

--- JS_files/credits.js
+++ JS_files/credits.js
@@ -1,4 +1,5 @@
 'use strict';
+// Tutorial I used is https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements
 
 customElements.define('credits-scroll', CreditsScroll, { extends: 'div' });
 

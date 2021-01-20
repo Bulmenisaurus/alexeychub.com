9c2abb21646fdc3119951c0f8ca6d0a5f4e6f96d
a <alexeybulmeni@gmail.com>
2021-01-20 13:27:05 -0800

    Initial commit

---
+++ JS_files/credits.js
@@ -0,0 +1,9 @@
+'use strict';
+
+customElements.define('credits-scroll', CreditsScroll, { extends: 'div' });
+
+class CreditsScroll extends HTMLDivElement {
+    constructor() {
+        super();
+    }
+}

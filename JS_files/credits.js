d682e914b2de1062d63bd8643d8a9c0676dc9a6d
a <alexeybulmeni@gmail.com>
2021-01-20 16:49:17 -0800

    Added functionality to credit-scroll element

--- JS_files/credits.js
+++ JS_files/credits.js
@@ -1,10 +1,15 @@
 'use strict';
 // Tutorial I used is https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements
 
-customElements.define('credits-scroll', CreditsScroll, { extends: 'div' });
-
 class CreditsScroll extends HTMLDivElement {
     constructor() {
-        super();
+        const me = super();
+        me.style.transitionDuration = me.dataset.scrollTime;
+        console.log(me.dataset);
+        me.style.transitionTimingFunction = me.dataset.transitionStyle ? me.dataset.transitionStyle : 'linear';
     }
+
+
 }
+
+customElements.define('credits-scroll', CreditsScroll, { extends: 'div' });

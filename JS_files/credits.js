4523df5217f988f6ecbe55ae27725fd5a01bfb1e
a <alexeybulmeni@gmail.com>
2021-01-21 17:09:28 -0800

    Extended animation length in multiple ways

--- JS_files/credits.js
+++ JS_files/credits.js
@@ -11,7 +11,7 @@ class CreditsScroll extends HTMLDivElement {
         setTimeout(function() {
             // I just love how simple transitions make this <3
             document.querySelector('body > div:nth-child(1)').style.top = '-1600px';
-        }, 1000);
+        }, 200);
     }
 
 

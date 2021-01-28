772592270c4039918ee78007401533bdee2b3ee3
a <alexeybulmeni@gmail.com>
2021-01-28 15:22:04 -0800

    Add more content

--- JS_files/cssslider.js
+++ JS_files/cssslider.js
@@ -15,10 +15,23 @@ const actions = [
     ['h1', 'fontSize', '5vw'],
     ['h1', 'marginBottom', '0'],
     ['code', 'borderRadius', '3px'],
-    ['p', 'lineHeight', '125%'],
+    ['p', 'lineHeight', '150%'],
     ['#label', 'fontWeight', 'bold'],
     ['#label', 'textTransform', 'uppercase'],
     ['#label', 'fontSize', '13px'],
+    ['body', 'color', 'rgba(0, 0, 0, 0.8)'],
+    ['kbd', 'display', 'inline-block'],
+    ['kbd', 'margin', '0 .1em'],
+    ['kbd', 'padding', '.1em .6em'],
+    ['kbd', 'fontSize', '11px'],
+    ['kbd', 'lineHeight', '16.5px'],
+    ['kbd', 'color', 'rgb(36, 39, 41)'],
+    ['kbd', 'textShadow', '0 1px 0 white'],
+    ['kbd', 'backgroundColor', 'rgb(228, 230, 232)'],
+    ['kbd', 'border', '1px solid rgb(159, 166, 173)'],
+    ['kbd', 'borderRadius', '3px'],
+    ['kbd', 'boxShadow', '0 1px 1px rgba(12, 13, 14, 0.15), inset 0 1px 0 0 #fff'],
+    ['kbd', 'overflow-wrap', 'break-word'],
 ];
 document.querySelector('input').max = actions.length;
 
@@ -54,7 +67,7 @@ function cssanimation() {
         for (const [x, element] of elementsAffected.entries()) {
 
             if (!x) {
-                document.getElementsByTagName('code')[0].innerHTML = `<strong>document.querySelectorAll('${style[0]}')</strong> style.${style[1]} = ${style[2]}`;
+                document.getElementsByTagName('code')[0].innerHTML = `<b>'${style[0]}'</b>: style.${style[1]} = ${style[2]}`;
             }
 
             element.style[style[1]] = style[2];

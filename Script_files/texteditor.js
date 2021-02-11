60d161c94784678c611215fda57b7036595dda0b
a <alexeybulmeni@gmail.com>
2021-02-11 08:45:32 -0800

    Renamed JS_files to Script_files

--- Script_files/texteditor.js
+++ Script_files/texteditor.js
@@ -1,22 +1,14 @@
-const textEditor: HTMLCanvasElement = document.querySelector('canvas');
-const ctx: CanvasRenderingContext2D = textEditor.getContext('2d');
-
+const textEditor = document.querySelector('canvas');
+const ctx = textEditor.getContext('2d');
 ctx.translate(0.5, 0.5);
 ctx.imageSmoothingEnabled = false;
-
 let canvasText = 'Hello!';
-
 const clearCanvas = () => ctx.clearRect(0, 0, textEditor.width, textEditor.height);
-
-
+const handleKey = function (e, text) {
+    return '';
+};
 document.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-        canvasText = canvasText.slice(0, -1)
-    } else if (e.key.length === 1) {
-        canvasText += e.key;
-    }
-
-
+    canvasText = handleKey(e, canvasText);
     clearCanvas();
     ctx.font = '20px monspace';
     ctx.fillText(canvasText, 10, 100, textEditor.width - 20);

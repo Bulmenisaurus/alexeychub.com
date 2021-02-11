fde8748a413318d3da09c5e7f29876b2931f9bf6
a <alexeybulmeni@gmail.com>
2021-02-10 22:41:21 -0800

    Simple key handler

--- JS_files/texteditor.js
+++ JS_files/texteditor.js
@@ -1,2 +1,17 @@
 const textEditor = document.querySelector('canvas');
-const ctx = textEditor;
+const ctx = textEditor.getContext('2d');
+ctx.translate(0.5, 0.5);
+ctx.imageSmoothingEnabled = false;
+let canvasText = 'Hello!';
+const clearCanvas = () => ctx.clearRect(0, 0, textEditor.width, textEditor.height);
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+        canvasText = canvasText.slice(0, -1);
+    }
+    else if (e.key.length === 1) {
+        canvasText += e.key;
+    }
+    clearCanvas();
+    ctx.font = '20px monspace';
+    ctx.fillText(canvasText, 10, 100, textEditor.width - 20);
+});
--- JS_files/texteditor.ts
+++ JS_files/texteditor.ts
@@ -1,2 +1,23 @@
 const textEditor: HTMLCanvasElement = document.querySelector('canvas');
-const ctx = textEditor
+const ctx: CanvasRenderingContext2D = textEditor.getContext('2d');
+
+ctx.translate(0.5, 0.5);
+ctx.imageSmoothingEnabled = false;
+
+let canvasText = 'Hello!';
+
+const clearCanvas = () => ctx.clearRect(0, 0, textEditor.width, textEditor.height);
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+        canvasText = canvasText.slice(0, -1)
+    } else if (e.key.length === 1) {
+        canvasText += e.key;
+    }
+
+
+    clearCanvas();
+    ctx.font = '20px monspace';
+    ctx.fillText(canvasText, 10, 100, textEditor.width - 20);
+});

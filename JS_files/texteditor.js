0a75df38fc7d325082d65f1cff7a7fddd97bebd6
a <alexeybulmeni@gmail.com>
2021-02-11 12:18:48 -0800

    Some attempts at text wrapping

--- JS_files/texteditor.js
+++ JS_files/texteditor.js
@@ -2,10 +2,10 @@ const textEditor = document.querySelector('canvas');
 const ctx = textEditor.getContext('2d');
 ctx.translate(0.5, 0.5);
 ctx.imageSmoothingEnabled = false;
-let canvasText = 'Hello!';
+let canvasText = 'Hello! Hi! Mom! Dad!';
 console.log('test');
 const clearCanvas = () => ctx.clearRect(0, 0, textEditor.width, textEditor.height);
-const handleKey = function (e, text) {
+const handleKey = (e, text) => {
     let newText = text;
     if (e.key === 'Backspace') {
         newText = canvasText.slice(0, -1);
@@ -18,14 +18,20 @@ const handleKey = function (e, text) {
     }
     return newText;
 };
+const createLineBreaks = (text) => text.split('\n');
+const wrap = (text) => {
+    return text.map(t => (t.match(/(.{5})/g) || [t]).join('\n'));
+};
+const drawText = (text, canvasCtx) => {
+    for (const [i, line] of text.entries()) {
+        canvasCtx.fillText(line, 10, i * 20 + 20);
+    }
+};
 document.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-        canvasText = canvasText.slice(0, -1);
-    }
-    else if (e.key.length === 1) {
-        canvasText += e.key;
-    }
+    canvasText = handleKey(e, canvasText);
+    const splitText = createLineBreaks(canvasText);
+    const wrappedText = wrap(splitText);
     clearCanvas();
     ctx.font = '20px monspace';
-    ctx.fillText(canvasText, 10, 100, textEditor.width - 20);
+    drawText(wrappedText, ctx);
 });
--- JS_files/texteditor.ts
+++ JS_files/texteditor.ts
@@ -4,37 +4,48 @@ const ctx: CanvasRenderingContext2D = textEditor.getContext('2d');
 ctx.translate(0.5, 0.5);
 ctx.imageSmoothingEnabled = false;
 
-let canvasText = 'Hello!';
+let canvasText = 'Hello! Hi! Mom! Dad!';
 
 console.log('test');
 
 const clearCanvas = () => ctx.clearRect(0, 0, textEditor.width, textEditor.height);
 
-const handleKey = function(e: KeyboardEvent, text: string): string {
+const handleKey = (e: KeyboardEvent, text: string) => {
     let newText = text;
 
     if (e.key === 'Backspace') {
         newText = canvasText.slice(0, -1);
-    }
-    else if (e.key === 'Enter') {
+    } else if (e.key === 'Enter') {
         newText += '\n'
-    }
-    else if (e.key.length === 1) {
+    } else if (e.key.length === 1) {
         newText += e.key;
     }
 
     return newText;
 }
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-        canvasText = canvasText.slice(0, -1)
-    } else if (e.key.length === 1) {
-        canvasText += e.key;
-    }
+const createLineBreaks = (text: string) => text.split('\n');
 
+
+const wrap = (text: string[]) => {
+    return text.map(t => (t.match(/(.{5})/g) || [t]).join('\n'));
+};
+
+const drawText = (text: string[], canvasCtx: CanvasRenderingContext2D) => {
+    for (const [i, line] of text.entries()) {
+        canvasCtx.fillText(line, 10, i * 20 + 20);
+    }
+}
+
+
+document.addEventListener('keydown', (e) => {
+    canvasText = handleKey(e, canvasText);
+
+    const splitText = createLineBreaks(canvasText);
+
+    const wrappedText = wrap(splitText);
 
     clearCanvas();
     ctx.font = '20px monspace';
-    ctx.fillText(canvasText, 10, 100, textEditor.width - 20);
+    drawText(wrappedText, ctx);
 });

3db76689c31c3ac25c88edbf877c5a3f869684b4
a <alexeybulmeni@gmail.com>
2021-02-11 09:03:27 -0800

    Nvm renaming broke everything

--- JS_files/texteditor.js
+++ JS_files/texteditor.js
@@ -1,35 +1,30 @@
-const textEditor: HTMLCanvasElement = document.querySelector('canvas');
-const ctx: CanvasRenderingContext2D = textEditor.getContext('2d');
-
+const textEditor = document.querySelector('canvas');
+const ctx = textEditor.getContext('2d');
 ctx.translate(0.5, 0.5);
 ctx.imageSmoothingEnabled = false;
-
 let canvasText = 'Hello!';
-
 console.log('test');
-
 const clearCanvas = () => ctx.clearRect(0, 0, textEditor.width, textEditor.height);
-
-const handleKey = function(e: KeyboardEvent, text: string): string {
+const handleKey = function (e, text) {
     let newText = text;
-
     if (e.key === 'Backspace') {
         newText = canvasText.slice(0, -1);
     }
     else if (e.key === 'Enter') {
-        newText += '\n'
+        newText += '\n';
     }
     else if (e.key.length === 1) {
         newText += e.key;
     }
-
     return newText;
-}
-
+};
 document.addEventListener('keydown', (e) => {
-    canvasText = handleKey(e, canvasText)
-
-
+    if (e.key === 'Backspace') {
+        canvasText = canvasText.slice(0, -1);
+    }
+    else if (e.key.length === 1) {
+        canvasText += e.key;
+    }
     clearCanvas();
     ctx.font = '20px monspace';
     ctx.fillText(canvasText, 10, 100, textEditor.width - 20);

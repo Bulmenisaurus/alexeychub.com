e5be87ad172a1d89292efac26df6f8f53546fe34
a <alexeybulmeni@gmail.com>
2021-02-18 20:55:45 -0800

    Valentine previewing too

--- scripts/onelinewonders.js
+++ scripts/onelinewonders.js
@@ -1,5 +1,5 @@
 /* global hljs */
-const [bName, bPrev, vName, vMessage] = ['bday-recipient', 'bday-preview', 'valentine-name', 'valentine-message'].map((e) => document.getElementById(e));
+const [bName, bPrev, vName, vMessage, vPrev] = ['bday-recipient', 'bday-preview', 'valentine-name', 'valentine-message', 'valentine-preview'].map((e) => document.getElementById(e));
 
 const cleanStr = (str) => JSON.stringify(str).slice(1, -1).replace(/'/gm, '\\\'');
 
@@ -12,11 +12,18 @@ bName.onkeyup = function() {
     bPrev.innerText = Array(4).fill(['Happy birthday ', '', '']).map((a, p) => a.map((a, e) => e == 1 && p == 2 ? `dear ${name}` : e == 1 && p != 2 ? 'to you' : e == 2 ? ',,!.'[p] : a)).map(a => a.join('')).concat('\n' + '\uD83C\uDF81\uD83C\uDF89'.repeat(5)).join('\n');
 };
 
+bName.onkeyup();
+
 vName.onkeyup = vMessage.onkeyup = function() {
     const name = cleanStr(vName.value);
     const message = cleanStr(vMessage.value);
 
     // eslint-disable-next-line no-useless-escape
-    document.getElementById('v-day').innerText = `console.log((function ____(_, __, ___) { return \`\\n      _____           _____\\n, ad8PPPP88b,     , d88PPPP8ba, \\n d8P\"      \"Y8b, ,d8P\"      \"Y8b\\ndP'           \"8a8\"           \\\`Yd\\n8(              \"              )8\\nI8                             8I\\n Yb, \${___(_, 23)} ,dP\\n  \"8a, \${___(__, 19)} ,a8\"\\n    \"8a,                 ,a8\"\\n      \"Yba             adP\"\\n        \\\`Y8a         a8P'\\n          \\\`88,     ,88'\\n            \"8b   d8\"\\n             \"8b d8\"\\n              \\\`888'\\n                \"\\n\\n\\n\`; })('${message}', '${name}', function(t, e) { const a = (e - t.length) \/ 2; return t.padStart(Math.ceil(e - a), ' ') + ' '.repeat(Math.ceil(a)) }))`;
+    document.getElementById('v-day').innerText = `console.log((function ____(_, __, ___) { return \`      _____           _____\\n, ad8PPPP88b,     , d88PPPP8ba, \\n d8P\"      \"Y8b, ,d8P\"      \"Y8b\\ndP'           \"8a8\"           \\\`Yd\\n8(              \"              )8\\nI8                             8I\\n Yb, \${___(_, 23)} ,dP\\n  \"8a, \${___(__, 19)} ,a8\"\\n    \"8a,                 ,a8\"\\n      \"Yba             adP\"\\n        \\\`Y8a         a8P'\\n          \\\`88,     ,88'\\n            \"8b   d8\"\\n             \"8b d8\"\\n              \\\`888'\\n                \"\`; })('${message}', '${name}', function(t, e) { const a = (e - t.length) \/ 2; return t.padStart(Math.ceil(e - a), ' ') + ' '.repeat(Math.ceil(a)) }))`;
     hljs.highlightBlock(document.getElementById('v-day'));
+
+    // eslint-disable-next-line
+    vPrev.innerText = (function ____(_, __, ___) { return `      _____           _____\n  ,ad8PPPP88b,     ,d88PPPP8ba,\n d8P"      "Y8b, ,d8P"      "Y8b\ndP'           "8a8"           \`Yd\n8(              "              )8\nI8                             8I\n Yb, ${___(_, 23)} ,dP\n  "8a, ${___(__, 19)} ,a8"\n    "8a,                 ,a8"\n      "Yba             adP"\n        \`Y8a         a8P'\n          \`88,     ,88'\n            "8b   d8"\n             "8b d8"\n              \`888'\n                "`; })('Happy valentines day', 'mom', function(t, e) { const a = (e - t.length) / 2; return t.padStart(Math.ceil(e - a), ' ') + ' '.repeat(Math.ceil(a)) })
 };
+
+vName.onkeyup();

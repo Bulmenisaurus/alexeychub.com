0d58cd5d1844803428bdc09e9029ccfe3b72ab05
alexey <alexeybulmeni@gmail.com>
2021-05-02 13:19:58 -0700

    Convert 404.js to TS and start work on most similar page

--- scripts/404.ts
+++ scripts/404.ts
@@ -1,14 +1,19 @@
-const strHash = (str, max) => {
+const strHash = (str: string, max: number) => {
+
     let hash = 0;
-    if (str.length == 0)
-        return hash;
+
+    if (str.length == 0) return hash;
+
     for (let i = 0; i < str.length; i++) {
         const char = str.charCodeAt(i);
         hash = ((hash << 5) - hash) + char;
         hash = hash & hash;
     }
+
     return Math.abs(hash) % max + 1;
 };
+
+
 const urlHash = strHash(window.location.href, 5);
 console.log(urlHash);
 const messages = [
@@ -17,32 +22,58 @@ const messages = [
     'Oh no! This page doesn\'t seem to exist!\nAnyway..',
     'Ouch, it looks like this page doesn\'t exist.',
     'Sadly, a 404 error occured. I wonder if you can collect the next card in the series, a 405, too.',
-    'This page doesn\'t exist. Oh well!'
-];
+    'This page doesn\'t exist. Oh well!'];
+
 document.getElementById('404-message').innerText = messages[urlHash];
-const fileExtension = (file) => {
+
+interface GithubApiFile {
+    path: string,
+    mode: string,
+    type: string,
+    size: number,
+    url: string
+}
+
+interface GithubApiTree {
+    sha: string,
+    url: string,
+    tree: GithubApiFile[],
+}
+
+
+const fileExtension = (file: string) => {
     return file.split('.').pop();
-};
-const getAllRepoFiles = async (owner, repo) => {
+}
+
+const getAllRepoFiles = async (owner: string, repo: string) => {
     // https://stackoverflow.com/questions/25022016/get-all-file-names-from-a-github-repo-through-the-github-api
     const body = await fetch(`api.github.com/repos/${owner}/${repo}/git/commits/master`);
-    return JSON.parse(body.toString());
-};
-const repoFiles = async (owner, repo, fileTypes) => {
-    const files = (await getAllRepoFiles(owner, repo)).tree;
+    return <GithubApiTree>JSON.parse(body.toString());
+}
+
+const repoFiles = async (owner: string, repo: string, fileTypes: string[]) => {
+    const files = (await getAllRepoFiles(owner, repo)).tree
+
     return files.filter(i => fileTypes.includes(fileExtension(i.path)));
-};
+}
+
 // https://stackoverflow.com/a/36566052/13996389
-const similarity = (s1, s2) => {
-    const [shorter, longer] = [s1, s2].sort((a, b) => a.length - b.length);
+
+const similarity = (s1: string, s2: string): number => {
+
+    const [shorter, longer] = [s1, s2].sort((a, b) => a.length - b.length)
+
     if (longer.length == 0) {
         return 1.0;
     }
+
     return (longer.length - editDistance(longer, shorter)) / longer.length;
-};
-const editDistance = (s1, s2) => {
+}
+
+const editDistance = (s1: string, s2: string) => {
     s1 = s1.toLowerCase();
     s2 = s2.toLowerCase();
+
     var costs = new Array();
     for (var i = 0; i <= s1.length; i++) {
         var lastValue = i;
@@ -53,7 +84,8 @@ const editDistance = (s1, s2) => {
                 if (j > 0) {
                     var newValue = costs[j - 1];
                     if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                        newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                        newValue = Math.min(Math.min(newValue, lastValue),
+                            costs[j]) + 1;
                     costs[j - 1] = lastValue;
                     lastValue = newValue;
                 }
@@ -63,7 +95,10 @@ const editDistance = (s1, s2) => {
             costs[s2.length] = lastValue;
     }
     return costs[s2.length];
-};
-const mostSimilarSitePage = async (page) => {
+}
+
+
+const mostSimilarSitePage = async (page: string) => {
     const files = await repoFiles('Bulmenisaurus', 'bulmenisaurus.github.io', ['html']);
-};
+
+}
